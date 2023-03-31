3b5b5fb92c817ee764c42e2048d980b272afe8a9
battleheart70 <battleheart70@gmail.com>
2023-04-01 01:42:11 +0200

    FirstCommit

---
+++ JavaProject/newporj/src/main/java/Locators3.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators3 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/pavelphilipchik/Study/chromedriver_mac_arm64/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        
+    }
+}
